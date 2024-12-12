dab009c5b1d24fd416f6848f281010b72d955e85
Ana Lívia da Costa Pessoa <analiviapessoa@uol.com.br>
2024-12-12 11:41:49 -0300

    Escolha do tempo

---
+++ app-tempo/src/App.jsx
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+import logo from "./assets/logo.png"
+
+import "./App.css";
+
+const TimeSelection = () => {
+  const times = [
+    { label: "Curto", value: "1h" },
+    { label: "1h30", value: "1:30h" },
+    { label: "2h", value: "2h" },
+    { label: "2h30", value: "2:30h" },
+    { label: "3h", value: "3h" },
+    { label: "Longo", value: "3+h" },
+  ];
+
+  const [selectedTime, setSelectedTime] = useState(times[0].value);
+
+  const handleTimeClick = (time) => {
+    setSelectedTime(time.value);
+  };
+
+  return (
+    <div className="container">
+
+      <div className="left-section">
+
+        <div className="logo">
+          <img src={logo} alt="Logo" className="logo-image" />
+        </div>
+
+        <h1 className="title">
+          Escolha <br /> do Tempo <br /> Disponível
+        </h1>
+      </div>
+
+      <div className="right-section">
+
+        <div className="timeline">
+          {times.map((time, index) => (
+            <div
+              key={index}
+              className={`time-point ${
+                selectedTime === time.value ? "selected" : ""
+              }`}
+              onClick={() => handleTimeClick(time)}
+            >
+              <div className="time-label">{time.label}</div>
+            </div>
+          ))}
+        </div>
+        
+      </div>
+
+      <button className="nav-button-left">←</button>
+      <button className="nav-button-right">→</button>
+
+    </div>
+  );
+};
+
+export default TimeSelection;
